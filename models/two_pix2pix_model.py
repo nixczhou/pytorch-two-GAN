b1bcc4f32666d6a208760c681ca0313d3da70651
jimmy <jimmy@westlake.cs.ubc.ca>
2018-03-09 16:32:21 -0800

    debug

--- models/two_pix2pix_model.py
+++ models/two_pix2pix_model.py
@@ -128,7 +128,7 @@ class TwoPix2PixModel:
             fake_B = util.tensor2im(self.fake_B.data)
             fake_C = util.tensor2im(self.fake_C.data)
             real_C = util.tensor2im(self.real_C.data)
-            masked_A = util.tensor2im(self.masked_A)
+            masked_A = util.tensor2im(self.masked_A.data)
             return OrderedDict([('real_A', real_A), ('fake_B', fake_B), ('fake_C', fake_C), ('real_C', real_C)],
         ['masked_A', masked_A])           
             
